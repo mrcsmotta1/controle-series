3001d289c47917fab3cf2535271388c5c19d0fb6
Marcos Motta <mrcsmotta1@gmail.com>
2023-08-09 13:49:19 -0300

    Creating Controller with artisan

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -1,9 +1,13 @@
 <?php
+
 namespace App\Http\Controllers;
 
-class SeriesController
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
 {
-    public function listarSeries() {
+    public function index()
+    {
         $series = [
             'Punisher',
             'Lost',
