513090f599d6c7c0dc5442ff1cd6af8032007125
Marcos Motta <mrcsmotta1@gmail.com>
2023-08-09 12:52:17 -0300

    Create first controller and route

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Http\Controllers;
+
+class SeriesController
+{
+    public function listarSeries() {
+        $series = [
+            'Punisher',
+            'Lost',
+            'Grey\'s Anatomy'
+        ];
+
+        $html = '<ul>';
+        foreach ($series as $serie) {
+            $html .= "<li>$serie</li>";
+        }
+        $html .= "</ul>";
+
+        echo $html;
+    }
+}
