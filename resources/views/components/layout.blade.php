a9b42598e16eb033173d08fb02155dccc90b9d80
Marcos Motta <mrcsmotta1@gmail.com>
2023-08-10 11:22:09 -0300

    Installing bootstrap and dependencies

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,7 +11,7 @@
     <title>{{ $title }}</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
