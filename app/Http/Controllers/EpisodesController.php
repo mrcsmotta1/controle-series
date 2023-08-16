8fcd457a4af4fde0dfbf682f94e06c8af70f3158
Marcos Motta <mrcsmotta1@gmail.com>
2023-08-16 18:34:31 -0300

    showing success message when marking episode as watched

--- app/Http/Controllers/EpisodesController.php
+++ app/Http/Controllers/EpisodesController.php
@@ -10,7 +10,10 @@ class EpisodesController extends Controller
 {
     public function index(Season $season)
     {
-        return view('episodes.index', ['episodes' => $season->episodes]);
+        return view('episodes.index', [
+            'episodes' => $season->episodes,
+            'mensagemSucesso' => session('mensagem.sucesso')
+        ]);
     }
 
     public function update(Request $request, Season $season)
@@ -21,6 +24,7 @@ class EpisodesController extends Controller
         });
         $season->push();
 
-        return to_route('episodes.index', $season->id);
+        return to_route('episodes.index', $season->id)
+            ->with('mensagem.sucesso', 'Episódios marcados como assistidos');
     }
 }
