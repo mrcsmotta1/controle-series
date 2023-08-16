5f7b042e6363ea869fc902f7e2d7a57dc7aa0692
Marcos Motta <mrcsmotta1@gmail.com>
2023-08-16 18:16:24 -0300

    Creating episode list watched

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\EpisodesController;
 use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,21 +15,21 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return redirect('series');
 });
 
-Route::get('/ola', function(){
+Route::get('/ola', function () {
     return 'Olá Mundo!';
 });
 
 Route::resource('/series', SeriesController::class)
-->except('show');
+    ->except('show');
 
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
 
+Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
 
-
-
+Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('update.index');
