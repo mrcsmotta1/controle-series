d507fa163e4d287ccc57c55d4af5b8665c763223
Marcos Motta <mrcsmotta1@gmail.com>
2023-08-17 19:38:11 -0300

    Logout link creation

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,7 @@ Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('signin');
+Route::get('/logout',[LoginController::class, 'destroy'])->name('logout');
 
 Route::get('/register', [UsersController::class, 'create'])->name('users.create');
 Route::post('/register', [UsersController::class, 'store'])->name('users.store');
