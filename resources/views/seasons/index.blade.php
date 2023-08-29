2e2fb07aa39dfc1ad40f65e566e4dfda9b4ef991
Marcos Motta <mrcsmotta1@gmail.com>
2023-08-29 18:54:54 -0300

    Creating and displaying cover image

--- resources/views/seasons/index.blade.php
+++ resources/views/seasons/index.blade.php
@@ -1,6 +1,8 @@
 <x-layout title="Temporadas de {!! $series->nome !!}">
-
-
+    <div class="text-center">
+        <img src="{{ asset('storage/' . $series->cover) }}" style="height: 200px"
+            alt="Capa da série {{ !! $series->nome }}" class="img-fluid">
+    </div>
     <ul class="list-group">
         @foreach ($seasons as $season)
         <li class="list-group-item d-flex justify-content-between align-items-center">
