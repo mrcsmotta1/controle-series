8304c0285175b6624eb25275f9397c1ef93231fc
Marcos Motta <mrcsmotta1@gmail.com>
2023-09-05 13:16:23 -0300

    Creation of the first series api endpoint

---
+++ app/Http/Controllers/Api/SeriesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SeriesFormRequest;
+use App\Models\Series;
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+        return Series::all();
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        return response()->json(Series::create($request->all()), 201);
+    }
+}
