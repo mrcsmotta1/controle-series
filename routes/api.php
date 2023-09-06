9db74cf8623a762ffad782a76a0b3c3ff31809d6
Marcos Motta <mrcsmotta1@gmail.com>
2023-09-06 20:04:55 -0300

    JWT Token Creation

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\SeriesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,13 +14,17 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/series', SeriesController::class);
-Route::get('/series/{series}/seasons', [SeriesController::class, 'storeSeason']);
-Route::get('/series/{series}/episodes', [SeriesController::class, 'storeEpisodes']);
-Route::patch('/episodes/{episode}/watch',[SeriesController::class, 'watchEpisodes']);
+Route::post('/login', [LoginController::class, 'index']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/series', SeriesController::class);
+    Route::get('/series/{series}/seasons', [SeriesController::class, 'storeSeason']);
+    Route::get('/series/{series}/episodes', [SeriesController::class, 'storeEpisodes']);
+    Route::patch('/episodes/{episode}/watch', [SeriesController::class, 'watchEpisodes']);
+});
